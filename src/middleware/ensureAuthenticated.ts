ac2f939f18dc3ce3ab84ee922790b30710464bc9
Renan <renan.fachin@hotmail.com>
2023-01-04 22:56:50 -0300

    override express types

--- src/middleware/ensureAuthenticated.ts
+++ src/middleware/ensureAuthenticated.ts
@@ -1,6 +1,10 @@
 import { Request, Response, NextFunction } from 'express'
 import { verify } from 'jsonwebtoken'
 
+// É necessário criar esta interface em função de ter sobrescrito as tipagens do express
+interface IPayload {
+    sub: string;
+}
 
 export function ensureAuthenticated(request: Request, response: Response, next: NextFunction){
     // Capturando o token
@@ -23,11 +27,13 @@ export function ensureAuthenticated(request: Request, response: Response, next: 
 
     // Fazendo um try catch para tratar erros
     try{
-        const { sub } = verify(token, process.env.JWT_SECRET)
+        const { sub } = verify(token, process.env.JWT_SECRET) as IPayload
 
         // Adicionando os dados do usuário dentro de um request
         request.user_id = sub        
 
+        return next();
+        
     } catch(e){
         return response.status(401).json({errorCode: "token.expired"})
     }
